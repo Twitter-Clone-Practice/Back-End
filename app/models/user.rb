ab256a0386acb9a0c8f6d6a5bb3e12a9507528e5
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-09-30 18:58:19 -0400

    Created models for User, Following and Follower
    Co-authored-by: Jose Lopez <joselopez11394@gmail.com>

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+    has_many :followings
+    has_many :following, through: :followings
+
+    has_many :followers
+    has_many :follower, through: :followers
+
+    validates :username, :email, uniqueness: true, presence: true
+    validates :password, :date_of_birth, presence: true
+end
