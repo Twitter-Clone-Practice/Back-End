567713813d3f9eefd05cb2161b535ad93ce753dd
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-01 19:36:21 -0400

    Fixed mutation to create user

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,33 +1,35 @@
-class Mutation::CreateUser < BaseMutation
-  # description "this will create new users"
+module Mutations
+  class CreateUser < BaseMutation
 
-  argument :username, String, required: true 
-  argument :email, String, required: true 
-  argument :password, String, required: true 
-  argument :password_digest, String, required: true 
-  argument :date_of_birth, String, required: true 
+    field :user, Types::UserType, null: true
+    field :errors, [String], null: false
 
-  field :user, Types::UserType, null: false
-  field :errors, [String], null: false
+    argument :username, String, required: true
+    argument :password, String, required: true
+    argument :password_confirmation, String, required: true
+    argument :email, String, required: true
+    argument :date_of_birth, String, required: true
 
-  def resolve(username:, email:, password:, password_digest:, date_of_birth:)
-    user = User.new(username: username, 
-                    email: email,
-                    password: password,
-                    password_digest: password_digest,
-                    date_of_birth: date_of_birth
-    )
+    def resolve(args)
+      user = User.new(
+        username: args[:username],
+        password: args[:password],
+        password_digest: args[:password_confirmation],
+        email: args[:email],
+        date_of_birth: args[:date_of_birth]
+      )
 
-    if user.save
-      {
-        user: user,
-        errors: []
-      }
-    else
-      {
-        user: nil,
-        errors: user.errors.full_messages
-      }
+      if user.save
+        { 
+          user: user,
+          errors: []
+        }
+      else
+        {
+          user: nil,
+          errors: user.errors.full_messages
+        }
+      end
     end
   end
 end
