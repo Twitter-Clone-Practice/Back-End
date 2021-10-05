cc2926c0e8984070eca01dc846310d52b19700c3
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-05 19:17:19 -0400

    Changed the has many followings and followers to what it should be users
    Co-authored-by: Jose Lopez <joselopez11394@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :followings
-    has_many :following, through: :followings
+    has_many :users, through: :followings
 
     has_many :followers
-    has_many :follower, through: :followers
+    has_many :users, through: :followers
 
     validates :username, :email, uniqueness: true, presence: true
     validates :password, :date_of_birth, presence: true
