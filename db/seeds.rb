7b4e94c7932a74ff3f13ef80efe452f5cef2b32a
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-11 19:09:11 -0400

    Created seeds for quality of life

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_eternal = User.create(username: 'Eternal', email: "Eternal@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "05/14/1990")
+User.create(username: 'Crimson', email: "Crimson@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "03/05/1998")
+Post.create(user_id: user_eternal.id, message: "cool")
