953ade40c1338305d636bf2ba38a6d4fbabc9780
Jose Lopez <joselopez11394@gmail.com>
2021-10-11 16:53:55 -0400

    adds a field for posts in the user type and created a new post type
    Co-authored-by: Roberto Basulto <Roberto.Basulto085@gmail.com>

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class PostType < Types::BaseObject
+    field :id, ID, null: false
+    field :user_id, Integer, null: true
+    field :message, String, null: true
+    field :number_of_likes, Integer, null: true
+    field :number_of_replys, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
