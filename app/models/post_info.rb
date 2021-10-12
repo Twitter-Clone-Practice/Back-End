9689321a4a15b959c70742a5edcf1983c93b54b8
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-12 18:50:50 -0400

    Added a has many relationship to comments

--- app/models/post_info.rb
+++ app/models/post_info.rb
@@ -2,5 +2,7 @@ class PostInfo < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  has_many :comments, class_name: 'Comment', foreign_key: :post_info_id
+
   validates_uniqueness_of :user, scope: :post_id
 end
