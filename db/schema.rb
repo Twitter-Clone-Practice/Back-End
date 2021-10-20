b42db94687d5492e113fc55b02201bcd56303f18
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-20 17:33:35 -0400

    Created comments table, model and spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_191159) do
+ActiveRecord::Schema.define(version: 2021_10_20_204454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.string "body"
+    t.integer "parent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "followers", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -53,6 +64,8 @@ ActiveRecord::Schema.define(version: 2021_10_11_191159) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "followers", "users"
   add_foreign_key "followers", "users", column: "follower_id"
   add_foreign_key "followings", "users"
