5c77ecb320d3658934325495c76b119a5617e2df
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-21 16:57:51 -0400

    Created mutation for delete_comment

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :delete_comment, mutation: Mutations::DeleteComment
     field :new_comment, mutation: Mutations::NewComment
     field :new_post, mutation: Mutations::NewPost
     field :follow_user, mutation: Mutations::FollowUser
