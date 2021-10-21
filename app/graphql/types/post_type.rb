5d9e09d36187edb118aba78704b4c0e60e01bfeb
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-20 21:01:01 -0400

    Added the CommentType in the PostType as a field called comments

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,8 +4,14 @@ module Types
     field :user_id, Integer, null: true
     field :message, String, null: true
     field :number_of_likes, Integer, null: true
-    field :number_of_replys, Integer, null: true
+    field :number_of_comments, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :comments, [CommentType], null: false
+
+    def comments
+      object.comments.where(parent_id: nil)
+    end
   end
 end
