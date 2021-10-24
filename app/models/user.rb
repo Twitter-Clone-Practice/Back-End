80fbff66556cdfc9a5bc55495ffa3bb373de399c
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-24 03:48:56 -0400

    Updated post and user model with has_meny likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
     
     has_many :followings
     has_many :following, through: :followings
-    has_many :posts
+    has_many :posts, dependent: :destroy
 
     has_many :followers
     has_many :follower, through: :followers
+
+    has_many :likes, dependent: :destroy
 
     validates :username, :email, uniqueness: true, presence: true
     validates :password, :date_of_birth, presence: true
