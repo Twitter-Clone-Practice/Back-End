04add40d1c828cb5b48c09fb2f2fe8e9c9130137
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-24 03:47:17 -0400

    Updated seeds file with new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+#Users
 user_eternal = User.create!(username: 'Eternal', email: "Eternal@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "05/14/1990")
 user_crimson = User.create!(username: 'Crimson', email: "Crimson@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "03/05/1998")
-post = Post.create!(user_id: user_eternal.id, message: "First Post")
+
+#Posts
+post = Post.create!(user_id: user_eternal.id, message: "First Post", number_of_comments: 2, number_of_likes: 1)
+second_post = Post.create!(user_id: user_eternal.id, message: "Second Post", number_of_comments: 2, number_of_likes: 1)
+crimson_post = Post.create!(user_id: user_crimson.id, message: "Cool", number_of_comments: 0, number_of_likes: 1)
+
+#Comments
 first_comment = Comment.create!(post_id: post.id, user_id: user_crimson.id, body: "Comments work")
 Comment.create!(post_id: post.id, body: "Replies work", user_id: user_eternal.id, parent_id: first_comment.id)
+
+#Likes
+Like.create!(post_id: post.id, user_id: user_crimson.id)
+Like.create!(post_id: second_post.id, user_id: user_crimson.id)
+Like.create!(post_id: crimson_post.id, user_id: user_eternal.id)
