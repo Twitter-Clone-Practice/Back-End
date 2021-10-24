a263eab1edbc5f9bc43b9e440563b05eef184202
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-10-23 22:06:59 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 
 user_eternal = User.create!(username: 'Eternal', email: "Eternal@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "05/14/1990")
 user_crimson = User.create!(username: 'Crimson', email: "Crimson@gmail.com", password: '123', password_confirmation: '123', date_of_birth: "03/05/1998")
-post = Post.create!(user_id: user_eternal.id, message: "First Post")
+post = Post.create!(user_id: user_eternal.id, message: "First Post", number_of_comments: 2)
 first_comment = Comment.create!(post_id: post.id, user_id: user_crimson.id, body: "Comments work")
 Comment.create!(post_id: post.id, body: "Replies work", user_id: user_eternal.id, parent_id: first_comment.id)
